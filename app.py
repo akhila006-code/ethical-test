bd8429da8ad9883bddd991e26b5f747f3c5d75ef
akhila006-code <142368475+akhila006-code@users.noreply.github.com>
2024-03-15 19:24:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request,render_template
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/", methods=["GET","POST"])
 def index():
@@ -28,5 +28,5 @@ def answer():
 def end():
         return(render_template("end.html"))
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     app.run()
